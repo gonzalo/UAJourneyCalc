e646035240d2e1d8c422c5b4f3225c2f1c6d7a63
Gonzalo Cao Cabeza de Vaca <gonzalo.cao@gmail.com>
2014-06-10 15:07:44 +0200

    First calculations unstable version

--- UAJourneyCalc.js
+++ UAJourneyCalc.js
@@ -146,4 +146,56 @@ function updateEvent(caller_id, value){
                                 break;
 
     }
+    
+    //después de actualizados los valores correspondientes calculamos los tiempos
+    updateResults();
 }
+
+function updateResults(){
+    //calculamos el número de minutos que debes cumplir semanalmente
+    
+    // 1. convertimos horario a minutos y lo ponemos en la salida correspondiente
+    var jornada_array = $('#jornada_normal').val().split(":");
+    var n_dias = parseInt($('#dias_habiles').val());
+    var reduccion = parseInt($('#reduccion').val());
+    var total_minutos_semanales = (parseInt(jornada_array[0])*60+parseInt(jornada_array[1])-reduccion)*n_dias;
+    
+    $('#total_horas').val(minutes2hours_str(total_minutos_semanales));
+    
+    // 2.
+    
+    // 1. obtenemos un date de referencia para hacer el cálculo
+    var fecha_referencia = new Date(); //pondremos el día de hoy a las 00:00
+    fecha_referencia.setHours(0);
+    fecha_referencia.setMinutes(0);
+    fecha_referencia.setSeconds(0);
+    fecha_referencia.setMilliseconds(0);
+    
+    
+    return true;
+}
+
+function minutes2hours_str(minutes_amount){
+    
+    var hours = Math.floor(minutes/60)
+    var minutes = minutes % 60
+    return(hours & ":" & minutes);
+    
+    
+}
+
+// Date differences calculator
+// return the difference between two dates using the unit indicated as 'datepart'
+// http://www.htmlgoodies.com/html5/javascript/calculating-the-difference-between-two-dates-in-javascript.html#fbid=6XgWpf3AF8C
+// datepart: 'y', 'm', 'w', 'd', 'h', 'n', 's'
+Date.dateDiff = function(datepart, fromdate, todate) {	
+  datepart = datepart.toLowerCase();	
+  var diff = todate - fromdate;	
+  var divideBy = { w:604800000, 
+                   d:86400000, 
+                   h:3600000, 
+                   n:60000, 
+                   s:1000 };	
+  
+  return Math.floor( diff/divideBy[datepart]);
+};
